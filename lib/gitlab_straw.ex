291179e539d587468dcf47815d01477deebf2af5
ohyecloudy <ohyecloudy@gmail.com>
2018-06-06 08:28:24 +0900

    :star: mix new --sup

---
+++ lib/gitlab_straw.ex
@@ -0,0 +1,18 @@
+defmodule GitlabStraw do
+  @moduledoc """
+  Documentation for GitlabStraw.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GitlabStraw.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gitlab_straw_test.exs
@@ -0,0 +1,8 @@
+defmodule GitlabStrawTest do
+  use ExUnit.Case
+  doctest GitlabStraw
+
+  test "greets the world" do
+    assert GitlabStraw.hello() == :world
+  end
+end
